0f6c9ee82f4c5364e5cebae830fe43cc1c27962f
sudhakarbastawade2303 <112871403+sudhakarbastawade2303@users.noreply.github.com>
2023-11-01 12:24:08 +0530

    Update security.tf

--- user-data/security.tf
+++ user-data/security.tf
@@ -1,1 +1,30 @@
+resource "aws_security_group" "sg-tf_3" {
+  name        = "first-sg_3"
+  description = "Allow TLS inbound traffic"
 
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "TF-sg_3"
+  }
+}
