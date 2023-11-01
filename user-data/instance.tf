249b6751d8523019371126a0969f67b8096fafba
sudhakarbastawade2303 <112871403+sudhakarbastawade2303@users.noreply.github.com>
2023-11-01 12:22:22 +0530

    Update instance.tf

--- user-data/instance.tf
+++ user-data/instance.tf
@@ -1,1 +1,23 @@
+resource "aws_instance" "third-instance-tf" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = "second-key"
+  security_groups = ["first-sg_3"]
+  tags = {
+    Name = "third_ec2_tf"
+  }
+  user_data = file("${path.module}/script.sh")
 
+
+  provisioner "file" {
+    source      = "/home/ubuntu/user_data/demo"
+    destination = "/home/ubuntu/demo"
+      connection {
+        type        = "ssh"
+        user        = "ubuntu"
+        private_key = file("${path.module}/id_rsa")
+        host        = self.public_ip
+      }
+  }
+
+}
