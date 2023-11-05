fd6e5d421c056eba6e6d0d358eafb4b425a339e8
sudhakarbastawade2303 <112871403+sudhakarbastawade2303@users.noreply.github.com>
2023-11-05 09:13:50 +0530

    Update main.tf

--- aws_infra/main.tf
+++ aws_infra/main.tf
@@ -13,6 +13,6 @@ module "ec2" {
 
 module "vpc" {
   source = "./modules/vpc"
-  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnet_cidrs = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
