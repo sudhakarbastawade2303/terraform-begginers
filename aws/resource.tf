d826be170dd898875b21523678f48421b7b631a9
sudhakarbastawade2303 <112871403+sudhakarbastawade2303@users.noreply.github.com>
2025-09-27 07:27:42 +0530

    Update resource.tf

--- aws/resource.tf
+++ aws/resource.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "key-tf" {
   key_name   = "first-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCB/C6kgc2dnvZNigClCBcDwms2LaxEPD7Vk0dnMShotW57ZTL2pE3II6n55UU/HrLTX/7vSsaxqNts5b5PE8VEwpv8VLZe5TkMVNLz+Sl2e+7HbRbR0B5ZRDIHQ2Lj1W4aKrWARTYhpHxI+BfLztDKdkX4EaftKBitqgHbgc7mAvip2HSXwUF1iyqCTFr8ug5y7aM+ERnOPaPcDji0SMseZroPQ/VqRU/4uGoyvO23Vl4vG/PmsN8ZFUMY9O5ifuquVRSzABPLYKJI3cDQMeOr3NiAy+nMWBa17WagPbnWnBskBeTx1dGsTPHFvkWqjCzx2lLLXZV4OjOTN/kBPSTVLND/wUGPw67gzn2oBHnCR3nkkD45NL2nLJK+uBwXXjOoIHuHMxXzeW8Q7Me27voMr1UgnG9L490rp3xkvUxaQqJLy7Z9pGkMQPjbRKWNdbPhuH7gCXwAGr8u0u+ese5zQdC9Ne21RoHd3fVyN8DaMnm2tdsIrEH9DsjuV1Ee/kU= ubuntu@ip-172-31-27-113"
+  public_key = ""
 }
 
 
